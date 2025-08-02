5c5ef1b006f3479c5e6faa6b0a9d9a4951d8ed71
huzaifa <muhammadhuzaifakhalidaziz@outlook.com>
2025-08-02 12:36:15 +0400

    Delete src directory

--- src/ai/persona.js
+++
@@ -1,9 +0,0 @@
-// Persona logic placeholder
-// Move persona logic from bot.js here for maintainability
-
-function getPersona(userId, username, prompt) {
-  // ...existing persona logic from bot.js...
-  // Return { systemPrompt, modelPrompt }
-}
-
-module.exports = getPersona;
--- src/utils/sanitizeReply.js
+++
@@ -1,8 +0,0 @@
-function sanitizeReply(content) {
-  return content
-    .replace(/@everyone/g, 'everyone')
-    .replace(/@here/g, 'here')
-    .replace(/<@&?\d+>/g, '[ping removed]');
-}
-
-module.exports = sanitizeReply;
